6fbcd77c7a6e43893e46a649a096038ad8badb10
Joao Veiga <joao.a.veiga@gmail.com>
2015-03-23 23:22:41 +0000

    set DEBUG to false

--- myproj/myproj/local_settings.py
+++ myproj/myproj/local_settings.py
@@ -1,8 +1,14 @@
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'test',
         'USER': 'test',
-        'PASSWORD': 'test'
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
     }
 }
